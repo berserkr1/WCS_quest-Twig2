5143ec714ff316fd75b56fb6e07bb2cb24fa922f
berserkr1 <g.coirier@epmistes.net>
2022-10-18 11:07:23 +0200

    quest challenge

--- public/index.php
+++ public/index.php
@@ -3,7 +3,17 @@
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
 
-
-$name = 'Wilder';
-
-echo $twig->render('home.html.twig', ['name' => $name]);
+if (isset($_GET['page'])) {
+    switch ($_GET['page']) {
+        case 'home':
+            echo $twig->render('home.html.twig');
+            break;
+        case 'layout':
+            echo $twig->render('layout.html.twig');
+            break;
+        default:
+            echo $twig->render('home.html.twig');
+    }
+} else {
+    echo $twig->render('home.html.twig');
+}
